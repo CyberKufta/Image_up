4961d7cee347a93045c3b6562f2aefbaf7510583
Muath S <muath.sharawi@gmail.com>
2014-05-13 09:40:45 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageUp::Application.config.secret_key_base = 'd3aea7e37738592e96d06fb132dfe5e00ab2261b34a6e5569bcd5454311ae6989185fd14ed79fa548626bde3134dbf0f711faf64a4e0d4ecb11e1fce514e1e20'
